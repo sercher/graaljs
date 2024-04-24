ea01148991128b823fd7e0a802924995c9f390ee
Sergey Chernyshev <schernyshev@openjdk.org>
2024-04-25 00:14:36 +0200

    bootstrap: lazy-load Performance.prototype.timeOrigin
    PR-URL: https://github.com/nodejs/node/pull/46425
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- graal-nodejs/lib/internal/process/pre_execution.js
+++ graal-nodejs/lib/internal/process/pre_execution.js
@@ -392,7 +392,6 @@ function setupTraceCategoryState() {
 }
 
 function setupPerfHooks() {
-  require('internal/perf/performance').refreshTimeOrigin();
   require('internal/perf/utils').refreshTimeOrigin();
 }
 
